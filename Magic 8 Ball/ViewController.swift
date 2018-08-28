2712e8b3496934cbb093a3335f76073e00fd3cca
DANIEL OREILLY <danieloreilly@DANIELs-MBP.fios-router.home>
2018-08-28 19:50:29 -0400

    Lecture 66

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -30,6 +30,12 @@ class ViewController: UIViewController {
         
     }
     
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+        newBallImage()
+        
+    }
+
 
     func newBallImage() {
         
