19fb67459594decc546e4bdc7ee75ba2283d35fd
DANIEL OREILLY <danieloreilly@DANIELs-MBP.fios-router.home>
2018-08-28 19:42:20 -0400

    Lecture 64

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let ballArray = ["ball1","ball2","ball3","ball4","ball5"]
+    
+    var randomBallNumber = 0
 
     @IBOutlet weak var imageView: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        randomBallNumber = Int.random(in: 0 ... 4)
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
 
     }
 
